1839830418cc6f0959e5f1f62caf0d98ebac5fd7
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2025-10-07 15:07:54 -0400

    initial commit

---
+++ Wobbily Wizard/Wobbily_WizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wobbily_WizardApp.swift
+//  Wobbily Wizard
+//
+//  Created by Jeb Barker on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Wobbily_WizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
