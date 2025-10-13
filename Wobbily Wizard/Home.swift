3a4998d6a04a96af9189b6b1850c4e0207c75a15
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2025-10-13 11:07:09 -0400

    Added healthkit code to track steps and display the progress in the bar at the top.

--- Wobbily Wizard/Home.swift
+++ Wobbily Wizard/Home.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import HealthKit
+import CoreMotion
 
 struct Home: View {
+    @State private var total : Double = 0.1
+    @StateObject private var stepCountModel = StepCountViewModel();
+    
     var body: some View {
         VStack {
-            LinearProgressView().frame(alignment: .top)
+            // set total equal to the percentage of steps taken towards the goal.
+            LinearProgressView(progress: stepCountModel.steps / 300, stepCountModel: stepCountModel).frame(alignment: .top)
+            Text("\(stepCountModel.steps)")
             Spacer()
             CircleRotation().frame(alignment: .bottom)
+        }.task {
+            //stepCountModel.startPedometerUpdates()
+            await stepCountModel.requestAuth()
         }
         .padding()
     }
@@ -21,7 +31,7 @@ struct Home: View {
 
 
 struct CircleRotation: View {
-    @State private var degrees: Double = 0
+    @State private var degrees: Double = 0;
 
     var body: some View {
         Image(systemName: "globe.americas")
@@ -37,18 +47,17 @@ struct CircleRotation: View {
 }
 
 struct LinearProgressView: View {
-    @State private var progress = 0.5
+    @State var progress : Double;
+    @StateObject var stepCountModel : StepCountViewModel;
 
     var body: some View {
         VStack {
-            ProgressView(value: progress)
+            ProgressView(value: stepCountModel.steps == -1 ? 0 : stepCountModel.steps, total: 300)
                 .progressViewStyle(PurplePotionProgressViewStyle())
             Button("More") {
-                if progress > 1 {
-                    progress = 0
-                }
-                else {
-                    progress += 0.05;
+                Task{
+                    try?
+                    await stepCountModel.fetchCumulativeSteps()
                 }
             }
         }
@@ -73,6 +82,103 @@ struct PurplePotionProgressViewStyle: ProgressViewStyle {
                     .foregroundColor(.purple)
             }.frame(height: barHeight) // have to limit height bc GeometryReader takes up all available space
         }
+    }
+}
+
+//class StepCountViewModel: ObservableObject {
+//    private let pedometer = CMPedometer()
+//    @Published var steps: Double = 0
+//
+//    func startPedometerUpdates() {
+//        guard CMPedometer.isPedometerEventTrackingAvailable() else {
+//            print("Pedometer event tracking not available.")
+//            return
+//        }
+//
+//        pedometer.startUpdates(from: Date()) { [weak self] (pedometerData, error) in
+//            DispatchQueue.main.async {
+//                if let pedometerData = pedometerData {
+//                    self?.steps = pedometerData.numberOfSteps.doubleValue
+//                } else if let error = error {
+//                    print("Pedometer error: \(error.localizedDescription)")
+//                }
+//            }
+//        }
+//    }
+//
+//    func stopPedometerUpdates() {
+//        pedometer.stopUpdates()
+//    }
+//}
+
+//extend date to quickly zero seconds out
+extension Date {
+    var zeroSeconds: Date? {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: self)
+        return calendar.date(from: dateComponents)
+    }
+}
+
+enum StepCounterError: Error {
+    case couldNotFetchHealthStore
+}
+
+final class StepCountViewModel: ObservableObject {
+
+    @Published var healthStore: HKHealthStore?
+    @Published var error: Error?
+    @Published var steps: Double = -1
+    @Published var startTime : Date = Date.now
+    
+    init() {
+        startTime = Date()
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+        } else {
+            error = StepCounterError.couldNotFetchHealthStore
+        }
+    }
+    
+    // Async function to request step count from HK
+    func requestAuth() async {
+        guard let stepCountType = HKQuantityType.quantityType(forIdentifier: .stepCount),
+              let healthStore else { return }
+        do {
+            try await healthStore.requestAuthorization(toShare: [], read: [stepCountType])
+        }
+        catch {
+            self.error = error
+        }
+    }
+    
+    // function to get steps from time that the view model was initialized
+    func fetchCumulativeSteps() async throws {
+        // make sure healthStore is initialized
+        guard let healthStore else { return }
+
+        let startDate = startTime
+        let healthStepType = HKQuantityType(.stepCount)
+        // Sample between the last time that this function was called and now.
+        let sampleDateRange = HKQuery.predicateForSamples(withStart: startDate, end: Date.now)
+        // build the argument that the query expects
+        let sample = HKSamplePredicate.quantitySample(type: healthStepType, predicate: sampleDateRange)
+        
+        let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: sample, options: .mostRecent, anchorDate: Date(), intervalComponents: DateComponents(day:1))
+
+        let stepsData = try await stepsQuery.result(for: healthStore)
+        
+        // sum the number of steps in each time period and store in the published steps variable
+        stepsData.enumerateStatistics(from: startDate.zeroSeconds ?? Date.now, to: Date.now) {
+            statistics, pointer in
+            let stepCount = statistics.sumQuantity()?.doubleValue(for: .count())
+            DispatchQueue.main.async {
+                if let stepCount, stepCount > 0 {
+                    self.steps = stepCount
+                }
+            }
+        }
+        print(self.steps)
     }
 }
 
