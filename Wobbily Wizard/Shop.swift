31ef0d01a708c539cd56f665983ed059a66f925f
Nick-Pleva <nicholas.pleva@uvm.edu>
2025-10-17 22:23:22 -0400

    Added shop tab

---
+++ Wobbily Wizard/Shop.swift
@@ -0,0 +1,26 @@
+//
+//  Shop.swift
+//  Wobbily Wizard
+//
+//  Created by Nick Pleva on 10/14/25.
+//
+
+import SwiftUI
+
+struct Shop: View {
+    let items = [["🍎", "Tomato"], ["🌿", "Leaf"], ["💎", ""], ["🐍", ""], ["🍔", ""], ["🍕", ""], ["🍜", ""], ["🌮", ""], ["🍣", ""], ["🥗", ""], ["💀", ""], ["🧪", ""], ["⛧", ""], ["🖤", ""], ["🕯️", ""], ["⚗️", ""]]
+    
+    var body: some View {
+        VStack {
+            
+        }
+        .frame(maxWidth: .infinity)
+        .background(
+            Image("Shop_Wizard_Back")
+                .resizable()
+                .scaledToFill()
+                .scaleEffect(1.25)
+                .ignoresSafeArea()
+        )
+    }
+}
