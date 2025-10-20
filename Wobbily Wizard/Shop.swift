62576c5d6c4118291f1cbbeaa3743138676a0687
Nick-Pleva <nicholas.pleva@uvm.edu>
2025-10-20 12:56:22 -0400

    Added balance textbox

--- Wobbily Wizard/Shop.swift
+++ Wobbily Wizard/Shop.swift
@@ -8,10 +8,12 @@ import SwiftUI
 
 struct Shop: View {
     let items = [["🍎", "Tomato"], ["🌿", "Leaf"], ["💎", ""], ["🐍", ""], ["🍔", ""], ["🍕", ""], ["🍜", ""], ["🌮", ""], ["🍣", ""], ["🥗", ""], ["💀", ""], ["🧪", ""], ["⛧", ""], ["🖤", ""], ["🕯️", ""], ["⚗️", ""]]
-    
+    let balance = 1234567 // Max balance will be set to 9,999,999 later
     var body: some View {
         VStack {
-            
+            Text("$\(balance)")
+                .offset(x: -104, y: -320)
+                .font(.custom("Noteworthy", size: 17))
         }
         .frame(maxWidth: .infinity)
         .background(
