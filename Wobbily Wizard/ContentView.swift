4ddbba4e92930f48b4d1aae8273ddcaabbc13990
Michael <m.m.jajkiewicz@gmail.com>
2025-10-28 15:07:28 -0400

    social tab slight redesign

--- Wobbily Wizard/ContentView.swift
+++ Wobbily Wizard/ContentView.swift
@@ -130,24 +130,38 @@ struct Cauldren: View {
 struct Friends: View{
     var body: some View{
         //hardcoded array of UIDs, ideally backend would exist to store usernames and UIDs
+        let userID = UUID()
         let id1 = UUID()
         let id2 = UUID()
         let id3 = UUID()
-        let arr = [id1, id2, id3]
+        let id4 = UUID()
+        let id5 = UUID()
+        let id6 = UUID()
+        let id7 = UUID()
+        let arr = [id1, id2, id3, id4, id5, id6, id7]
         VStack{
             Text("Friends List")
-            ForEach(arr, id: \.self){ userId in
-                ZStack{
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(.red)
-                        .frame(width: UIScreen.screenWidth * 0.85, height: UIScreen.screenHeight * 0.13, alignment: .center)
-                    VStack{
-                        Text("Nickname")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .padding()
-                        Text(userId.uuidString)
-                            .font(.caption)
+                .bold()
+                .font(.title2)
+            Text("Nickname")
+            Text(userID.uuidString)
+                .textSelection(.enabled)
+            let lightBlue = Color(red: 0, green: 50, blue: 186)
+            ScrollView{
+                ForEach(arr, id: \.self){ userId in
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(lightBlue)
+                            .frame(width: UIScreen.screenWidth * 0.85, height: UIScreen.screenHeight * 0.13, alignment: .center)
+                        VStack{
+                            Text("Nickname")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                                .padding()
+                            Text(userId.uuidString)
+                                .font(.caption)
+                                .textSelection(.enabled)
+                        }
                     }
                 }
                     
